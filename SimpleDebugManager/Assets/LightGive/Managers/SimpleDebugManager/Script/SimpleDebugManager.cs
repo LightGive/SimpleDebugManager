edd987f850b6f9ef349417c55b52f45b2e7cbd70
LightGive <roi.akase@outlook.jp>
2018-09-24 20:39:06 +0900

    新しくスクリプト追加、ManagerCreator追加

---
+++ SimpleDebugManager/Assets/LightGive/Managers/SimpleDebugManager/Script/SimpleDebugManager.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleDebugManager : LightGive.SingletonMonoBehaviour<SimpleDebugManager>
+{
+	private const float WidthPercent = 0.11f;
+	private const float WidthToHeightPercent = 0.46f;
+	private const float BlackAlpha = 0.5f;
+
+	[SerializeField]
+	private ShowDebugPosition m_debugPosition;
+	[SerializeField]
+	private bool m_isShow;
+	[SerializeField]
+	private Color normalFrameRateColor = Color.white;
+	[SerializeField]
+	private Color cautionFrameRateColor = Color.yellow;
+	[SerializeField]
+	private Color warningFrameRateColor = Color.red;
+
+	private double m_minFrameRate;
+	private double m_maxFrameRate;
+	private double m_frameRate;
+	private float m_elapsedTime;
+	private int m_frameCount;
+	private Canvas m_debugCanvas;
+	private Image m_imageBackgroundFPS;
+	private Text m_textFPS;
+
+	public enum ShowDebugPosition
+	{
+		TopRight,
+		TopLeft,
+		TopCenter,
+		BottomRight,
+		BottomLeft,
+		BottomCenter,
+	}
+
+	protected override void Awake()
+	{
+		isDontDestroy = true;
+		base.Awake();
+		CreateCanvas();
+	}
+
+	private void Update()
+	{
+
+	}
+
+
+	void CreateCanvas()
+	{
+		var canvasObject = new GameObject("DebugCanvas");
+		canvasObject.transform.SetParent(this.transform);
+		m_debugCanvas = canvasObject.AddComponent<Canvas>();
+		m_debugCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+		m_debugCanvas.sortingOrder = 9999;
+
+		var backgroundFpsObject = new GameObject("BackgroundFPS");
+		backgroundFpsObject.transform.SetParent(canvasObject.transform);
+		m_imageBackgroundFPS = backgroundFpsObject.AddComponent<Image>();
+
+		if (true)//Screen.width > Screen.height)
+		{
+			var wid = Screen.width * WidthPercent;
+			var hei = wid * WidthToHeightPercent;
+			m_imageBackgroundFPS.rectTransform.sizeDelta = new Vector2(wid, hei);
+			m_imageBackgroundFPS.color = new Color(0.0f, 0.0f, 0.0f, BlackAlpha);
+		}
+		else
+		{
+		}
+
+		SetPosition();
+
+		var textObject = new GameObject("TextFPS");
+		textObject.transform.SetParent(backgroundFpsObject.transform);
+		m_textFPS = textObject.AddComponent<Text>();
+		m_textFPS.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+		m_textFPS.alignment = TextAnchor.MiddleCenter;
+		m_textFPS.fontStyle = FontStyle.Bold;
+		m_textFPS.resizeTextForBestFit = true;
+		m_textFPS.resizeTextMaxSize = 300;
+		m_textFPS.resizeTextMinSize = 5;
+		m_textFPS.rectTransform.sizeDelta = m_imageBackgroundFPS.rectTransform.sizeDelta;
+		m_textFPS.rectTransform.anchoredPosition = Vector2.zero;
+		m_textFPS.text = "120";
+	}
+
+	void SetPosition()
+	{
+		var left = -(Screen.width / 2.0f) + (m_imageBackgroundFPS.rectTransform.sizeDelta.x / 2.0f);
+		var right = (Screen.width / 2.0f) - (m_imageBackgroundFPS.rectTransform.sizeDelta.x / 2.0f);
+		var bottom = -(Screen.height / 2.0f) + (m_imageBackgroundFPS.rectTransform.sizeDelta.y / 2.0f);
+		var top = (Screen.height / 2.0f) - (m_imageBackgroundFPS.rectTransform.sizeDelta.y / 2.0f);
+
+		switch (m_debugPosition)
+		{
+			case ShowDebugPosition.TopLeft: m_imageBackgroundFPS.rectTransform.anchoredPosition = new Vector2(left, top); break;
+			case ShowDebugPosition.TopRight: m_imageBackgroundFPS.rectTransform.anchoredPosition = new Vector2(right, top); break;
+			case ShowDebugPosition.TopCenter: m_imageBackgroundFPS.rectTransform.anchoredPosition = new Vector2(0.0f, top); break;
+			case ShowDebugPosition.BottomLeft: m_imageBackgroundFPS.rectTransform.anchoredPosition = new Vector2(left, bottom); break;
+			case ShowDebugPosition.BottomRight: m_imageBackgroundFPS.rectTransform.anchoredPosition = new Vector2(right, bottom); break;
+			case ShowDebugPosition.BottomCenter: m_imageBackgroundFPS.rectTransform.anchoredPosition = new Vector2(0.0f, bottom); break;
+		}
+	}
+
+
+	/// <summary>
+	/// FPSを計算する
+	/// </summary>
+	void CalcFramePerSecound()
+	{
+		m_frameCount++;
+		m_elapsedTime += Time.deltaTime;
+		if (m_elapsedTime > 0.5f)
+		{
+			m_frameRate = System.Math.Round(m_frameCount / m_elapsedTime, 1, System.MidpointRounding.AwayFromZero);
+
+			if (m_frameRate < 20)
+			{
+				m_textFPS.color = cautionFrameRateColor;
+			}
+			else if (m_frameRate < 40f)
+			{
+				m_textFPS.color = warningFrameRateColor;
+			}
+			else
+			{
+				m_textFPS.color = normalFrameRateColor;
+			}
+
+			if (m_minFrameRate > m_frameRate && Time.realtimeSinceStartup > 2f)
+				m_minFrameRate = m_frameRate;
+			else if (m_maxFrameRate < m_frameRate)
+				m_maxFrameRate = m_frameRate;
+
+			m_frameCount = 0;
+			m_elapsedTime = 0;
+		}
+	}
+}
---
+++ SimpleDebugManager/Assets/LightGive/Managers/SimpleDebugManager/Script/_DebugManager.cs
@@ -0,0 +1,256 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using LightGive;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// デバッグ関係をまとめたマネージャー
+/// </summary>
+public class _DebugManager : LightGive.SingletonMonoBehaviour<_DebugManager>
+{
+	public enum ShowDebugPosition
+	{
+		TopRight,
+		TopLeft,
+		TopCenter,
+		BottomRight,
+		BottomLeft,
+		BottomCenter,
+	}
+
+	[SerializeField]
+	private bool isShow = false;
+
+	[Header("FPS")]
+	[SerializeField]
+	private ShowDebugPosition showDebugPositon;
+	[SerializeField]
+	private bool slowFrameRateBreak = false;
+	[SerializeField]
+	private int targetFrameRate = 60;
+	[SerializeField]
+	private int fontSizeFrameRate = 50;
+	[SerializeField]
+	private int normalFrameRate = 30;
+	[SerializeField]
+	private int slowFrameRate = 10;
+	[SerializeField]
+	private Color normalFrameRateColor = Color.white;
+	[SerializeField]
+	private Color cautionFrameRateColor = Color.yellow;
+	[SerializeField]
+	private Color warningFrameRateColor = Color.red;
+
+	[Header("DebugText")]
+	[SerializeField]
+	private bool isShowBackground = false;
+	[SerializeField]
+	private int normalFontSize = 50;
+	[SerializeField]
+	private Color normalTextColor = Color.gray;
+	[SerializeField]
+	private string[] showTexts;
+
+	[Header("Setting")]
+	[SerializeField]
+	private KeyCode showDebugKeyCode = KeyCode.D;
+
+
+	private int frameCount;
+	private float elapsedTime;
+	private double frameRate;
+	private double minFrameRate;
+	private double maxFrameRate;
+	private GUIStyle fpsGuiStyle = new GUIStyle();
+	private GUIStyle normalTextGuiStyle = new GUIStyle();
+
+	private float FrameRateBoxWidth { get { return fontSizeFrameRate * 2.5f; } }
+	private float FrameRateBoxHeight { get { return fontSizeFrameRate * 1.2f; } }
+	private float NormalTextBoxHeight { get { return normalFontSize * 1.1f; } }
+
+
+	protected override void Awake()
+	{
+		base.Awake();
+		Application.targetFrameRate = targetFrameRate;
+
+		Init();
+	}
+
+	void Init()
+	{
+		fpsGuiStyle.alignment = TextAnchor.MiddleCenter;
+		fpsGuiStyle.fontStyle = FontStyle.Bold;
+		fpsGuiStyle.normal.textColor = Color.white;
+		frameRate = targetFrameRate;
+		maxFrameRate = 0;
+		minFrameRate = targetFrameRate;
+		normalTextGuiStyle.alignment = TextAnchor.MiddleLeft;
+	}
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update()
+	{
+		// FPSを計算する
+		CalcFramePerSecound();
+
+		//Debugキーを表示するかをチェックする
+		CheckDebugKey();
+
+	}
+
+	/// <summary>
+	/// FPSを計算する
+	/// </summary>
+	void CalcFramePerSecound()
+	{
+		frameCount++;
+		elapsedTime += Time.deltaTime;
+		if (elapsedTime > 0.5f)
+		{
+			frameRate = System.Math.Round(frameCount / elapsedTime, 1, System.MidpointRounding.AwayFromZero);
+
+			if (frameRate < 20)
+			{
+				//red
+			}
+			else if (frameRate < 40f)
+			{
+				//Color = yellow
+			}
+			else
+			{
+				//white
+			}
+
+			if (minFrameRate > frameRate && Time.realtimeSinceStartup > 2f)
+				minFrameRate = frameRate;
+			else if (maxFrameRate < frameRate)
+				maxFrameRate = frameRate;
+
+			frameCount = 0;
+			elapsedTime = 0;
+		}
+	}
+
+	/// <summary>
+	/// デバッグのキーをチェックする
+	/// </summary>
+	void CheckDebugKey()
+	{
+		//表示・非表示を切り替える
+		if (Input.GetKeyDown(showDebugKeyCode))
+		{
+			isShow = !isShow;
+		}
+	}
+
+	/// <summary>
+	/// Display
+	/// </summary>
+	private void OnGUI()
+	{
+		if (!isShow)
+			return;
+
+		//FPS表示
+		ShowFramePerSecound();
+		ShowNormalText();
+	}
+
+	/// <summary>
+	/// FPSを表示
+	/// </summary>
+	void ShowFramePerSecound()
+	{
+		fpsGuiStyle.fontSize = fontSizeFrameRate;
+
+		Rect debugFpsRect = Rect.zero;
+		switch (showDebugPositon)
+		{
+			case ShowDebugPosition.TopRight:
+				debugFpsRect = new Rect(Screen.width - FrameRateBoxWidth, 1, FrameRateBoxWidth, FrameRateBoxHeight);
+				break;
+			case ShowDebugPosition.TopLeft:
+				debugFpsRect = new Rect(1, 1, FrameRateBoxWidth, FrameRateBoxHeight);
+				break;
+			case ShowDebugPosition.BottomRight:
+				debugFpsRect = new Rect(Screen.width - FrameRateBoxWidth, Screen.height - FrameRateBoxHeight, FrameRateBoxWidth, FrameRateBoxHeight);
+				break;
+			case ShowDebugPosition.BottomLeft:
+				debugFpsRect = new Rect(1, Screen.height - FrameRateBoxHeight, FrameRateBoxWidth, FrameRateBoxHeight);
+				break;
+			default: break;
+		}
+
+		GUI.Box(debugFpsRect, "");
+		GUI.Label(debugFpsRect, frameRate.ToString("F1"), fpsGuiStyle);
+	}
+
+	/// <summary>
+	/// 通常のデバッグのテキストを表示する
+	/// </summary>
+	void ShowNormalText()
+	{
+		var maxLengh = 0;
+		for (int i = 0; i < showTexts.Length; i++)
+		{
+			if (showTexts[i].Length > maxLengh)
+			{
+				maxLengh = i;
+			}
+		}
+
+		normalTextGuiStyle.fontSize = normalFontSize;
+		normalTextGuiStyle.normal.textColor = normalTextColor;
+
+		var backgroundRect = new Rect(1, 1, GetTextBoxWidth(showTexts[maxLengh]), normalFontSize * showTexts.Length * 1.1f);
+		if (isShowBackground)
+			GUI.Box(backgroundRect, "");
+
+		for (int i = 0; i < showTexts.Length; i++)
+		{
+			var rect = new Rect(1, NormalTextBoxHeight * i, GetTextBoxWidth(showTexts[i]), normalFontSize);
+			GUI.Label(rect, showTexts[i], normalTextGuiStyle);
+		}
+	}
+
+	/// <summary>
+	/// 文字の幅を取得する
+	/// </summary>
+	/// <returns>The text box width.</returns>
+	/// <param name="_text">Text.</param>
+	float GetTextBoxWidth(string _text)
+	{
+		return normalFontSize * _text.Length * 1.0f;
+	}
+
+	/// <summary>
+	/// デバッグのテキストを追加して表示
+	/// </summary>
+	/// <param name="_text">Text.</param>
+	/// <param name="_line">Line.</param>
+	public void SetDebugText(string _text, int _line = 0)
+	{
+		_line = Mathf.Clamp(_line, 0, showTexts.Length);
+		showTexts[_line] = _text;
+	}
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(_DebugManager))]
+public class DebugManagerEditor : Editor
+{
+	private float timeScale = 0.0f;
+
+	public override void OnInspectorGUI()
+	{
+		base.DrawDefaultInspector();
+	}
+}
+#endif
